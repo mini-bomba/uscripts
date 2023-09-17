d4468989b85cd3acf94f4550b946d405e1c9ce96
mini-bomba <55105495+mini-bomba@users.noreply.github.com>
2023-09-17 17:10:28 +0200

    altclick-images: Run at document-start to bypass CSP, fix flickr

--- altclick-images.user.js
+++ altclick-images.user.js
@@ -2,22 +2,25 @@
 // @name        Alt-click to open all images
 // @namespace   uscripts.minibomba.pro
 // @description Opens all images under in the clicked element on alt-click
-// @version     1.1.0
+// @version     1.2.0
 // @match       *://*/*
 // @grant       GM_openInTab
 // @grant       GM_notification
+// @run-at      document-start
 // @updateURL   https://raw.githubusercontent.com/mini-bomba/uscripts/master/altclick-images.user.js
 // @downloadURL https://raw.githubusercontent.com/mini-bomba/uscripts/master/altclick-images.user.js
 // @author      mini_bomba
 // ==/UserScript==
 (function (){
   let last_size = null;
-  document.body.addEventListener("click", ev => {
+  document.addEventListener("click", ev => {
     if (!ev.altKey) return;
     ev.preventDefault();
     ev.stopImmediatePropagation();
-    // If parent element has the same size as current element, go up
     let target = ev.target;
+    // For flickr: if .photo-notes-scrappy-view was clicked, go up and find .photo-well-media-scrappy-view
+    if (target.classList.contains("photo-notes-scrappy-view")) target = target.parentElement.querySelector(":scope > .photo-well-media-scrappy-view") ?? target;
+    // If parent element has the same size as current element, go up
     while (target.parentElement != null && target.clientWidth === target.parentElement.clientWidth && target.clientHeight == target.parentElement.clientHeight) {
       target = target.parentElement;
     }
@@ -42,6 +45,6 @@
     if (!event.altKey) return;
     event.stopImmediatePropagation();
   }
-  document.body.addEventListener("mouseup", blockOnAlt, {capture: true});
-  document.body.addEventListener("mousedown", blockOnAlt, {capture: true});
+  document.addEventListener("mouseup", blockOnAlt, {capture: true});
+  document.addEventListener("mousedown", blockOnAlt, {capture: true});
 })();
