1a3ef19c0a307d350341db3877266402c7cf9e90
mini-bomba <55105495+mini-bomba@users.noreply.github.com>
2023-09-17 14:53:34 +0200

    Publish altclick-images.user.js

---
+++ README.md
@@ -0,0 +1,6 @@
+# mini_bomba's userscripts
+
+List of available userscripts:
+- [Alt-click to open all images](/altclick-images.user.js)
+
+All userscripts are released under the MIT license unless otherwise stated
---
+++ altclick-images.user.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name        Alt-click to open all images
+// @namespace   uscripts.minibomba.pro
+// @description Opens all images under in the clicked element on alt-click
+// @version     1.0
+// @match       *://*/*
+// @grant       GM_openInTab
+// @grant       GM_notification
+// @updateURL   https://raw.githubusercontent.com/mini-bomba/uscripts/master/altclick-images.user.js
+// @downloadURL https://raw.githubusercontent.com/mini-bomba/uscripts/master/altclick-images.user.js
+// @author      mini_bomba
+// ==/UserScript==
+(function (){
+  let last_size = null;
+  document.body.addEventListener("click", ev => {
+    if (!ev.altKey) return;
+    // Find all img elements under the element
+    const imgs = ev.target.querySelectorAll("img");
+    // Deduplicate urls
+    const urls = new Set();
+    if (ev.target.tagName.toLowerCase() === "img") urls.add(ev.target.src); // include target if it's an img element
+    for (const i of imgs) urls.add(i.src);
+    // Ask for confirmation when opening > 5 tabs
+    if (urls.size > 5 && last_size !== urls.size) {
+      last_size = urls.size;
+      GM_notification(`About to open ${urls.size} tabs. Alt-click again to continue.`);
+      return;
+    }
+    last_size = null;
+    // Open all images
+    for (const u of urls) if (u != null) GM_openInTab(u);
+  });
