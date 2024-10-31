295249c8f45acc3023b604baa1773faca5f5dbaa
mini-bomba <55105495+mini-bomba@users.noreply.github.com>
2024-10-31 19:13:47 +0100

    ytcg: add a fix for middle-clicking thumbnails

--- yt_custom_grid.user.js
+++ yt_custom_grid.user.js
@@ -1,20 +1,21 @@
 // ==UserScript==
-// @name        YouTube custom grid layouts
-// @namespace   uscripts.minibomba.pro
-// @match       *://*.youtube.com/*
-// @match       https://uscripts.minibomba.pro/yt_custom_grid
-// @grant       GM_getValue
-// @grant       GM_setValue
-// @grant       GM_registerMenuCommand
-// @grant       GM_openInTab
-// @require     https://raw.githubusercontent.com/mini-bomba/uscripts/master/utils/general.js
-// @require     https://raw.githubusercontent.com/mini-bomba/uscripts/master/utils/settings.js
-// @run-at      document-start
-// @version     1.0.1
-// @author      mini_bomba
-// @updateURL   https://raw.githubusercontent.com/mini-bomba/uscripts/master/yt_custom_grid.user.js
-// @downloadURL https://raw.githubusercontent.com/mini-bomba/uscripts/master/yt_custom_grid.user.js
-// @description Rewrites youtube's stupid rich "grid" renderer to actually use CSS grid, with custom values!
+// @name          YouTube custom grid layouts
+// @namespace     uscripts.minibomba.pro
+// @match         *://*.youtube.com/*
+// @match         https://uscripts.minibomba.pro/yt_custom_grid
+// @exclude-match https://accounts.youtube.com/*
+// @grant         GM_getValue
+// @grant         GM_setValue
+// @grant         GM_registerMenuCommand
+// @grant         GM_openInTab
+// @require       https://raw.githubusercontent.com/mini-bomba/uscripts/master/utils/general.js
+// @require       https://raw.githubusercontent.com/mini-bomba/uscripts/master/utils/settings.js
+// @run-at        document-start
+// @version       1.1.0
+// @author        mini_bomba
+// @updateURL     https://raw.githubusercontent.com/mini-bomba/uscripts/master/yt_custom_grid.user.js
+// @downloadURL   https://raw.githubusercontent.com/mini-bomba/uscripts/master/yt_custom_grid.user.js
+// @description   Rewrites youtube's stupid rich "grid" renderer to actually use CSS grid, with custom values!
 // ==/UserScript==
 
 // Configuration page boilerplate
@@ -24,6 +25,7 @@ const { SETTINGS, on_the_config_page } = settingsSetup({
     column_gap: "16px",
     row_gap: "40px",
     container_padding: "16px",
+    mmb_fix: true,
   },
   settings_page_name: "yt_custom_grid",
   settings_event_name: "mbusc-ytcg",
@@ -55,6 +57,28 @@ function add_css() {
   rule.style.setProperty("grid-column", "1 / -1");
 }
 
+function on_middleclick(ev) {
+  // only middle mouse button clicks processed
+  if (ev.button != 1) return;
+
+  // check enabled
+  if (!SETTINGS.mmb_fix) return;
+
+  // check that we didn't click a link
+  if (ev.target.closest("a") != null) return;
+
+  // check that we clicked a thumbnail
+  const thumbnail_item = ev.target.closest("ytd-rich-item-renderer");
+  if (thumbnail_item == null) return;
+  if (thumbnail_item.querySelector(":scope > div > yt-lockup-view-model") == null) return;
+
+  const title_element = thumbnail_item.querySelector(".yt-lockup-metadata-view-model-wiz__title")
+
+  window.open(title_element.href);
+  ev.preventDefault();
+  ev.stopImmediatePropagation();
+}
+
 function custom_setting_check(id, check) {
   document.getElementById(id).addEventListener("change", e => {
     if (isTag(event.target, "input")) {
@@ -73,6 +97,7 @@ function custom_setting_check(id, check) {
 if (!on_the_config_page) {
   GM_registerMenuCommand("Refresh CSS", add_css);
   add_css();
+  document.addEventListener("mousedown", on_middleclick, {capture: true});
 } else {
   waitUntilInteractive().then(() => {
     custom_setting_check("min-item-width", val => CSS.supports("grid-template-columns", `repeat(auto-fit, minmax(${val}, 1fr))`));
