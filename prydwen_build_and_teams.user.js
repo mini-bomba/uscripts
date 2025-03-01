f7e71f07abfc999ea50afa6bed7ee3223c606137
mini-bomba <55105495+mini-bomba@users.noreply.github.com>
2025-03-01 19:00:31 +0100

    prydwen_build_and_teams: use the vencord approach
    basically instead of hardcoding the module ID, regex search all module
    contents to find the right one instead.
    this should make the script less fragile

--- prydwen_build_and_teams.user.js
+++ prydwen_build_and_teams.user.js
@@ -3,13 +3,15 @@
 // @namespace   uscripts.minibomba.pro
 // @match       *://www.prydwen.gg/*
 // @grant       none
-// @version     1.0
+// @version     1.0.1
 // @author      mini_bomba
 // @run-at      document-start
 // @updateURL   https://raw.githubusercontent.com/mini-bomba/uscripts/master/prydwen_build_and_teams.user.js
 // @downloadURL https://raw.githubusercontent.com/mini-bomba/uscripts/master/prydwen_build_and_teams.user.js
 // @description Switches to the Build and Teams tab for characters automatically
 // ==/UserScript==
+
+const PLUGINS_MODULE_REGEX = /e\.exports\s*=\s*\[\s*{\s*plugin:/m
 
 let webpack_chunks = undefined;
 Object.defineProperty(window, 'webpackChunkprydwen', {
@@ -18,20 +20,21 @@ Object.defineProperty(window, 'webpackChunkprydwen', {
   },
   set(value) {
     webpack_chunks = value;
-    plugins_chunk = findWebpackChunk(webpack_chunks, 34741);
-    if (plugins_chunk != undefined) {
-      plugins_chunk[34741] = injectPlugins(plugins_chunk[34741])
+    // try to locate the plugins module
+    for (const chunk of webpack_chunks.map(c => c[1])) {
+      for (const module_id in chunk) {
+        if (PLUGINS_MODULE_REGEX.test(chunk[module_id].toString())) {
+          chunk[module_id] = injectPlugins(chunk[module_id])
+        }
+      }
     }
   }
 });
 
-function findWebpackChunk(chunks, module_id) {
-  return chunks.find(chunk => module_id in chunk[1])?.[1];
-}
-
 function injectPlugins(original_function) {
   if (original_function.toString().startsWith('function injected(')) return original_function;
   console.log("we got em");
+  window.original_plugins = original_function;
   function injected(module, wp_exports, wp_require) {
     window.mb_exports = wp_exports;
     window.mb_require = wp_require;
